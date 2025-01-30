5dce7d01d38348676216087cfc90b4f4110fc834
ZhuoEvan <156927177+ZhuoEvan@users.noreply.github.com>
2025-01-30 12:12:22 -0600

    Add hello-world program.

---
+++ eecs348_lab1.c
@@ -0,0 +1,12 @@
+//eecs348_lab1.c
+
+//Include Inputs | Outputs | Variables
+#include <stdio.h>
+
+//Main Function
+int main() {
+    //Print "hello world." into the terminal
+    printf("hello world.");
+    //Return nothing
+    return 0;
+}
