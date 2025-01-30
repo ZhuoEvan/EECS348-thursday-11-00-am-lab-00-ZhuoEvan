1583a466293d13c3651264729a28a294d86b2623
ZhuoEvan <156927177+ZhuoEvan@users.noreply.github.com>
2025-01-30 12:35:10 -0600

    Replace the hello-world program with the sample C program

--- eecs348_lab1.c
+++ eecs348_lab1.c
@@ -1,12 +1,25 @@
 //eecs348_lab1.c
 
-//Include Inputs | Outputs | Variables
 #include <stdio.h>
-
-//Main Function
+#include <string.h>
 int main() {
-    //Print "bye world." into the terminal
-    printf("bye world.");
-    //Return nothing
-    return 0;
+char str[100];
+int i, len;
+printf("Enter a string: ");
+gets(str);
+len = strlen(str);
+for (i = 0; i < len; i++) {
+if (str[i] >= 'a' && str[i] <= 'z') {
+str[i] = str[i] - 'a' + 'A';
+}
+}
+printf("The capitalized string is: %s\n", str);
+// Reverse the string.
+for (i = 0, len = strlen(str); i < len / 2; i++) {
+char temp = str[i];
+str[i] = str[len - 1 - i];
+str[len - 1 - i] = temp;
+}
+printf("The reversed string is: %s\n", str);
+return 0;
 }
